2aeb36991cf82083ba7dfa8d54131a0ea03064c4
YvanDroid <yvandroid@outlook.com>
2023-12-14 00:45:56 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,72 @@
+# This python file uses the following third party modules:
+# PyTube
+
+# To use, place this into the root folder of your albums or songs and run it.
+# Tested working on playlists from usdb.animux.de
+
+import os
+
+from pytube import YouTube
+
+cwd = os.getcwd()
+print(cwd)
+
+directory_list = [x[0] for x in os.walk(cwd)]
+print(directory_list)
+
+
+def song_download(folder):
+    # This function should use the folder name as the song name
+    current_folder = folder
+    song_name = os.path.basename(current_folder)
+    song_path = current_folder + "\\" + song_name
+    song_text_file = song_path + ".txt"
+    song_mp3 = song_path + ".mp3"
+    song_mp4 = song_path + ".mp4"
+    # This should be the song name
+    if os.path.isfile(song_mp4) or os.path.isfile(song_mp3):
+        print("Song files already downloaded")
+        return
+
+    try:
+        text_file = open(song_text_file, "r")
+    except OSError:
+        print("No file was found for this folder")
+        return
+
+    # Search the file for the #VIDEO header and check for youtube url
+    text_lines = text_file.readlines()
+    video_line = 0
+    for row in text_lines:
+        if row.find("#VIDEO") != -1:
+            video_line = row
+            break
+
+    # Should be the #VIDEO header. Hopefully no exceptions
+    video_id = video_line.split(",")[0][7:]
+    print(video_id)
+    yt_url = "http://youtube.com/watch?" + video_id
+    try:
+        yt = YouTube(yt_url)
+    except:
+        print(f"Song could not be found with the link for song {song_name} ")
+        return
+
+    # Get highest quality video out
+    try:
+        yt.streams.filter(progressive=True, file_extension="mp4").order_by(
+            "resolution"
+        ).desc().first().download(
+            output_path=current_folder, filename=song_name + ".mp4"
+        )
+        # Get the audio from the youtube video
+        yt.streams.filter(only_audio=True).first().download(
+            output_path=current_folder, filename=song_name + ".mp3"
+        )
+    except Exception as ex:
+        print(f"the following error has occured for {song_name} - {ex}")
+    text_file.close()
+
+
+for song in directory_list:
+    song_download(song)
